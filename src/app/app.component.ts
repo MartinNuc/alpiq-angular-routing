c54cae2b24bf444de403572601e8d48ad70a1eb3
Martin Nuc <martin@nuc.cz>
2019-07-18 13:53:19 +0200

    loading joke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { JokeService } from './joke.service';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { Joke } from './joke';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'jokes';
+export class AppComponent implements OnInit, AfterViewInit {
+  joke$: Observable<Joke>;
+
+  reload$ = new Subject();
+
+  constructor(public jokeService: JokeService) {}
+
+  ngOnInit() {
+    this.joke$ = this.reload$.pipe(
+      switchMap(() => this.jokeService.fetchRandomJoke())
+    );
+  }
+
+  ngAfterViewInit() {
+    this.reload();
+  }
+
+  reload() {
+    this.reload$.next();
+  }
 }
